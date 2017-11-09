2e9ed2a81d14f9f55365300d80f390d723b94f94
Josh Walwyn <me@joshwalwyn.com>
2017-11-09 23:35:42 +0000

    Initialised Django!

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+# from setuptools import setup, find_packages
+
+setup(
+    name='albatross',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
