6d521ad26887beb4aad8d7f9c09457d7c5ac4621
Josh Walwyn <me@joshwalwyn.com>
2017-11-12 22:21:34 +0000

    Added grunt copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,29 @@ module.exports = function(grunt) {
 
     },
     // Tasks to 'compile' project with bower components
-    compile: {
-
-    }
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            src: ['bower_components/admin-lite/dist/css/AdminLTE.min.css'],
+            cwd: 'src',
+            dest: 'static/vendor/admin-lite/'
+          },
+        ],
+      },
+    },
   });
 
   // Load Grunt tasks
   // Watch File Changes
   grunt.loadNpmTasks('grunt-contrib-watch')
+  // Copy files
+  grunt.loadNpmTasks('grunt-contrib-copy')
 
   // The default task (running 'grunt' in console) is 'watch'
   grunt.registerTask('default', ['watch'])
+
+  // Copy task, to move from bower_components to /static/vendor
+  grunt.registerTask('setup', ['copy'])
 }
