877ccc8c376f487f48f64ea35b805fa7729d7d1f
Josh Walwyn <me@joshwalwyn.com>
2018-01-14 20:27:23 +0000

    Webpacked library JS and minified

--- albatross.js
+++ albatross.js
@@ -1,3 +1,14 @@
-import Velocity from 'Velocity'
+// velocity-animation
+import Velocity from 'velocity-animate';
 
-// import main from 'AdminLite/build/js/main'
+// Bootstrap native
+import Bootstrap from 'bootstrap.native';
+
+// admin-lite
+import BoxRefresh from 'AdminLite/build/js/src/BoxRefresh';
+import BoxWidget from 'AdminLite/build/js/src/BoxWidget';
+import ControlSidebar from 'AdminLite/build/js/src/ControlSidebar';
+import DirectChat from 'AdminLite/build/js/src/DirectChat';
+import Layout from 'AdminLite/build/js/src/Layout';
+import PushMenu from 'AdminLite/build/js/src/PushMenu';
+import TodoList from 'AdminLite/build/js/src/TodoList';
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Babel = require('babel-loader');
 
 module.exports = {
   context: path.resolve(__dirname, '.'),
   entry: {
     'bundle': './albatross.js',
-    'bundle.min': './albatross.js'
+    'bundle.min': './albatross.js',
+    // 'bundle.min.css': [
+    //   './node_modules/bootstrap/dist/css/bootstrap.css'
+    // ],
   },
 
-  // Build Javascript
   output: {
     path: path.resolve(__dirname, 'static/js'),
     filename: 'albatross.[name].js',
@@ -19,15 +23,44 @@ module.exports = {
       // transpile ES6/7 to ES5 via babel
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
          loader: 'babel-loader',
          query: {
              presets: ['env']
          }
       },
-    ]
+    ],
+    // rules: [
+    //   {
+    //     test: /\.css$/,
+    //     use: ExtractTextPlugin.extract({
+    //       fallback: 'style-loader',
+    //       use: 'css-loader'
+    //     })
+    //   },
+    //   {
+    //     test: /\.scss$/,
+    //     loaders: ['style', 'css', 'postcss', 'sass']
+    //   },
+    //   {
+    //     test: /\.less$/,
+    //     loaders: ['style', 'css', 'less']
+    //   },
+    //   {
+    //     test: /\.woff$/,
+    //     loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+    //   },
+    //   {
+    //     test: /\.woff2$/,
+    //     loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+    //   },
+    //   {
+    //     test: /\.(eot|ttf|svg|gif|png)$/,
+    //     loader: "file-loader"
+    //   }
+    // ],
   },
 
+  // Uglify JS
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       include: /\.min\.js$/,
@@ -35,5 +68,15 @@ module.exports = {
     })
   ],
 
-  devtool: 'source-map'
+  devtool: 'source-map',
+
+  // Merge Library CSS
+  // - bootstrap
+  // - adminlite
+  //
+  // - fontawsome
+  // - ionicons
+  // plugins: [
+  //   new ExtractTextPlugin("bundle.min.css"),
+  // ],
 };
