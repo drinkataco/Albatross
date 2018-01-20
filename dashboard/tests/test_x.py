7797469b09aa79b6067c3cdd67cf8326b3176680
Josh Walwyn <me@joshwalwyn.com>
2018-01-20 21:36:44 +0000

    Trigger failed test

--- dashboard/tests/test_x.py
+++ dashboard/tests/test_x.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 class ExampleTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
-        self.assertEquals(True, True)
+        self.assertEquals(False, True)
